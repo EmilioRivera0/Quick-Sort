3265c7d2812d5d34aa5edfecb81039b3df4b932e
EmilioRivera0 <elcholula619@gmail.com>
2025-03-01 19:08:08 -0600

    Feature: Algorithm progress

--- lib/src/Quick_Sort.cpp
+++ lib/src/Quick_Sort.cpp
@@ -2,11 +2,22 @@
 
 // partition function prototype
 int partition(std::vector<int>& vec, int left, int right) {
+	// pivot index and index to lastly swapped element
 	int pivot{(left + right) / 2}, i{left - 1};
-	// sort vec
+	// sort vec from left to right in ascending order
 	for (int j{left}; j <= right; j++) {
+		// current element is less than or equal to pivot
+		if (vec[j] <= vec[pivot]) {
+			// update lastly swapped element index and swap elements
+			i++;
+			std::swap(vec[i], vec[j]);
+			// when pivot is reached, update its index
+			if (j == pivot) pivot = i;
+		}
+		// at the end, swap the pivot with the lastly swapped element
+		std::swap(vec[i], vec[pivot]);
+		pivot = i;
 	}
-	
 	return pivot;
 }
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
-﻿#include "lib/include/Quick_Sort.h"
+﻿#include <stdlib.h>
+#include <ctime>
+#include <iostream>
+
+#include "lib/include/Quick_Sort.h"
 
 int main()
 {
+	// generate random test vector to sort
+	srand(time(nullptr));
+	size_t size{ rand() % 100 };
+	std::vector<int> test_vec{};
+	for (int it{size}; it <= size; it++)
+		test_vec.push_back(rand() % 1000);
+
 	return 0;
 }
 
